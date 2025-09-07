ad0130ff7d43a4f4c0d915c83bd5bb43b328c415
Ansharora124 <ansharora13b@gmail.com>
2025-09-07 21:00:26 +0530

    props and component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+ 
+  
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+    <User name="gapu" age={25}/>
+    <User name="ansh" age={21} />
     </div>
+
+    </div>
+
+);
+
+}
+
+const User = (props)=>{   // thsi is  a component not fn and start from capital letters only
+  return (
+<div>
+      <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
+    </div>
+    
   );
 }
 
