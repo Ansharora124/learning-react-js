8365fb81d28904c07ba4123a2dd22238d378f089
Ansharora124 <ansharora13b@gmail.com>
2025-09-09 17:23:52 +0530

    ternary operators

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import styles from  './App.module.css';
+import  './App.css';
+const age=55;
+const isGreen=false;
+
+
 function App() {
  
   
-
   return (
     
-    <div className={styles.App}>
-     <h1 className={styles.name}> ansh</h1>
-
+    <div className="App">
+    {age>=18 ? <h1>adult </h1> : <h1>underage</h1>}
+ <h1 style={{color:isGreen ? "green": "red"}}>this is color </h1>
     </div>
 
 );
+
 
 }
 
